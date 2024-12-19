000e199b2a24256205fe5c2e00a86b8439336b7d
Mefamex <131618733+Mefamex@users.noreply.github.com>
2024-12-19 03:11:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import os
 import moviepy.editor as mp
-
-print("\n--------CURRENT PATH:\n\t=====",os.getcwd(),"\n")
 
 clip = mp.VideoFileClip(a.mp4")
 
 # make the height 360px ( According to moviePy documenation The width is then computed so that the width/height ratio is conserved.)
 clip_resized = clip.resize(height=720)
 
+clip_resized.write_videofile("movie_resized.mp4")
+
+
+""" ANOTHER WAY
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+clip = VideoFileClip("a.mp4")
+
+clip_resized = clip.resized(height=720)
 
 clip_resized.write_videofile("movie_resized.mp4")
+"""
